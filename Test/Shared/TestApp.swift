5beec91e2d198a171e2b84b2c5f38ffdcff4302e
Yukey Wang <yukey@smedia.com.au>
2022-12-21 12:38:04 +1100

    new build 1

---
+++ Test/Shared/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Shared
+//
+//  Created by Yukey Wang on 21/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
